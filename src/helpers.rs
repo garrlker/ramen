f658eab32ba1b9c6a721444de2a52d3117bbb700
viri <hi@viri.moe>
2020-12-21 00:44:35 -0600

    helpers: add `str_filter_nulls`

--- src/helpers.rs
+++ src/helpers.rs
@@ -2,6 +2,7 @@
 
 use std::sync::Arc;
 
+/// Used to `const` initialize fields which don't necessarily need allocation.
 #[derive(Clone)]
 pub enum MaybeStatic<T: ?Sized> {
     Static(&'static T),
@@ -12,5 +13,20 @@ impl<T: ?Sized> From<&'static T> for MaybeStatic<T> {
     #[inline]
     fn from(x: &'static T) -> Self {
         Self::Static(x)
+    }
+}
+
+/// Rust allows you to have null characters in your strings, which is pretty neat,
+/// but the native C APIs we have to interact with see it as a null *terminator*.
+///
+/// This function replaces all instances of null in a string with b' ' (space).
+pub fn str_filter_nulls(x: &mut String) {
+    // Safety: 0x00 and b' ' are one-byte sequences that can't mean anything else in UTF-8.
+    unsafe {
+        for byte in x.as_mut_vec() {
+            if *byte == 0x00 {
+                *byte = b' ';
+            }
+        }
     }
 }
