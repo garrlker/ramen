de0ff09886dd9fea5cfb82df256742539646f4b3
viri <hi@viri.moe>
2020-12-23 01:17:41 -0600

    error, platform: add `Internal(Error)` variant

--- src/error.rs
+++ src/error.rs
@@ -1,15 +1,34 @@
 //! Error types used within the crate.
 
+use crate::platform::imp;
 use std::fmt;
 
 #[derive(Debug)]
-pub struct Error {
-
+pub enum Error {
+    Internal(InternalError),
 }
 
+#[derive(Debug)]
+#[repr(transparent)]
+pub struct InternalError(imp::InternalError);
+
 impl std::error::Error for Error {}
+impl std::error::Error for InternalError {}
+
 impl fmt::Display for Error {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "TODO") // TODO
+        write!(f, "TODO") // TODO: !
+    }
+}
+
+impl fmt::Display for InternalError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        self.0.fmt(f)
+    }
+}
+
+impl Error {
+    pub(crate) fn from_internal(err: imp::InternalError) -> Error {
+        Error::Internal(InternalError(err))
     }
 }
--- src/platform.rs
+++ src/platform.rs
@@ -4,7 +4,11 @@
 // ===============
 //
 // To implement your own platforms, here's what you need to export:
-// - The type `WindowRepr` that derefs to or implements `WindowImpl` (is callable as such).
+// - The type `InternalError` which implements `std::error::Error` and is `Send + Sync`
+//   This is for OS error codes - you should construct these with `Error::from_internal`
+//
+// - The type `WindowRepr` that is callable as `WindowImpl` and is `Send + Sync`
+//
 // - The function `make_window` of type `fn(&WindowBuilder) -> Result<WindowRepr, Error>`
 
 #[cfg(windows)]
