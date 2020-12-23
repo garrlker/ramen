7ba2b573c0b86b8e8b58ff8c00635aec47e65f3c
viri <hi@viri.moe>
2020-12-23 00:23:57 -0600

    win32: add basic window that opens (and that's it)

---
+++ src/platform/bindings/win32.rs
@@ -0,0 +1,231 @@
+#![allow(bad_style, dead_code, overflowing_literals)]
+
+// Opaque handles
+macro_rules! def_handle {
+    // documented, exported
+    ($doc: literal, $name: ident, $private_name: ident $(,)?) => {
+        #[doc(hidden)]
+        pub enum $private_name {}
+        #[doc = $doc]
+        pub type $name = *mut $private_name;
+    };
+    // internal
+    ($name: ident, $private_name: ident $(,)?) => {
+        #[doc(hidden)]
+        pub enum $private_name {}
+        pub type $name = *mut $private_name;
+    };
+}
+def_handle!("Opaque handle to the executable file in memory.", HINSTANCE, HINSTANCE__);
+def_handle!("Opaque handle to a monitor.", HMONITOR, HMONITOR__);
+def_handle!("Opaque handle to a window.", HWND, HWND__);
+def_handle!(DPI_AWARENESS_CONTEXT, DPI_AWARENESS_CONTEXT__);
+def_handle!(HBRUSH, HBRUSH__);
+def_handle!(HDC, HDC__);
+def_handle!(HICON, HICON__);
+def_handle!(HMENU, HMENU__);
+def_handle!(HMODULE, HMODULE__);
+pub type HCURSOR = HICON;
+
+// Typedefs
+use core::ffi::c_void;
+pub type c_char = i8;
+pub type c_schar = i8;
+pub type c_uchar = u8;
+pub type c_short = i16;
+pub type c_ushort = u16;
+pub type c_int = i32;
+pub type c_uint = u32;
+pub type c_long = i32;
+pub type c_ulong = u32;
+pub type c_longlong = i64;
+pub type c_ulonglong = u64;
+pub type wchar_t = u16;
+pub type ATOM = WORD;
+pub type BOOL = c_int;
+pub type BYTE = c_uchar;
+pub type CHAR = c_char;
+pub type DWORD = c_ulong;
+pub type HANDLE = *mut c_void;
+pub type HRESULT = c_long;
+pub type INT = c_int;
+pub type LONG = c_long;
+pub type LONG_PTR = isize;
+pub type LPARAM = LONG_PTR;
+pub type LPCWSTR = *const WCHAR;
+pub type LPVOID = *mut c_void;
+pub type LRESULT = LONG_PTR;
+pub type UINT = c_uint;
+pub type UINT_PTR = usize;
+pub type ULONG_PTR = usize;
+pub type WCHAR = wchar_t;
+pub type WORD = c_ushort;
+pub type WPARAM = UINT_PTR;
+
+// Function typedefs
+pub type WNDPROC = unsafe extern "system" fn(HWND, UINT, WPARAM, LPARAM) -> LRESULT;
+
+// Constants
+pub const CS_OWNDC: UINT = 0x0020;
+pub const CW_USEDEFAULT: c_int = 0x80000000;
+pub const ERROR_SUCCESS: DWORD = 0; // lol
+pub const GWLP_USERDATA: c_int = -21;
+pub const WM_CREATE: UINT = 0x0001;
+pub const WM_DESTROY: UINT = 0x0002;
+pub const WM_CLOSE: UINT = 0x0010;
+pub const WM_NCCREATE: UINT = 0x0081;
+pub const WM_NCDESTROY: UINT = 0x0082;
+pub const WS_BORDER: DWORD = 0x00800000;
+pub const WS_CAPTION: DWORD = 0x00C00000;
+pub const WS_CHILD: DWORD = 0x40000000;
+pub const WS_CLIPCHILDREN: DWORD = 0x02000000;
+pub const WS_CLIPSIBLINGS: DWORD = 0x04000000;
+pub const WS_DISABLED: DWORD = 0x08000000;
+pub const WS_DLGFRAME: DWORD = 0x00400000;
+pub const WS_GROUP: DWORD = 0x00020000;
+pub const WS_HSCROLL: DWORD = 0x00100000;
+pub const WS_ICONIC: DWORD = WS_MINIMIZE;
+pub const WS_MAXIMIZE: DWORD = 0x01000000;
+pub const WS_MAXIMIZEBOX: DWORD = 0x00010000;
+pub const WS_MINIMIZE: DWORD = 0x20000000;
+pub const WS_MINIMIZEBOX: DWORD = 0x00020000;
+pub const WS_OVERLAPPED: DWORD = 0x00000000;
+pub const WS_OVERLAPPEDWINDOW: DWORD =
+    WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU | WS_THICKFRAME | WS_MINIMIZEBOX | WS_MAXIMIZEBOX;
+pub const WS_POPUP: DWORD = 0x80000000;
+pub const WS_SIZEBOX: DWORD = WS_THICKFRAME;
+pub const WS_SYSMENU: DWORD = 0x00080000;
+pub const WS_TABSTOP: DWORD = 0x00010000;
+pub const WS_THICKFRAME: DWORD = 0x00040000;
+pub const WS_TILED: DWORD = WS_OVERLAPPED;
+pub const WS_TILEDWINDOW: DWORD = WS_OVERLAPPEDWINDOW;
+pub const WS_VISIBLE: DWORD = 0x10000000;
+pub const WS_VSCROLL: DWORD = 0x00200000;
+
+// Structs
+#[repr(C)]
+pub struct POINT {
+    pub x: LONG,
+    pub y: LONG,
+}
+#[repr(C)]
+pub struct RECT {
+    pub left: LONG,
+    pub top: LONG,
+    pub right: LONG,
+    pub bottom: LONG,
+}
+#[repr(C)]
+pub struct MSG {
+    pub hwnd: HWND,
+    pub message: UINT,
+    pub wParam: WPARAM,
+    pub lParam: LPARAM,
+    pub time: DWORD,
+    pub pt: POINT,
+}
+#[repr(C)]
+pub struct WNDCLASSEXW {
+    pub cbSize: UINT,
+    pub style: UINT,
+    pub lpfnWndProc: WNDPROC,
+    pub cbClsExtra: c_int,
+    pub cbWndExtra: c_int,
+    pub hInstance: HINSTANCE,
+    pub hIcon: HICON,
+    pub hCursor: HCURSOR,
+    pub hbrBackground: HBRUSH,
+    pub lpszMenuName: LPCWSTR,
+    pub lpszClassName: LPCWSTR,
+    pub hIconSm: HICON,
+}
+
+// Statically linked imports
+#[link(name = "Kernel32")]
+extern "system" {
+    pub fn GetLastError() -> DWORD;
+    pub fn SetLastError(dwErrCode: DWORD);
+    pub fn ExitProcess(uExitCode: UINT);
+}
+#[link(name = "User32")]
+extern "system" {
+    // Window class management & global atom table
+    pub fn GetClassInfoExW(hinst: HINSTANCE, lpszClass: LPCWSTR, lpwcx: *mut WNDCLASSEXW) -> BOOL;
+    pub fn RegisterClassExW(lpWndClass: *const WNDCLASSEXW) -> ATOM;
+    pub fn GlobalFindAtomW(lpString: LPCWSTR) -> ATOM;
+
+    // Window management
+    pub fn CreateWindowExW(
+        dwExStyle: DWORD,
+        lpClassName: LPCWSTR,
+        lpWindowName: LPCWSTR,
+        dwStyle: DWORD,
+        x: c_int,
+        y: c_int,
+        nWidth: c_int,
+        nHeight: c_int,
+        hWndParent: HWND,
+        hMenu: HMENU,
+        hInstance: HINSTANCE,
+        lpParam: LPVOID,
+    ) -> HWND;
+    pub fn DestroyWindow(hWnd: HWND) -> BOOL;
+
+    // Message loop
+    pub fn DefWindowProcW(hWnd: HWND, Msg: UINT, wParam: WPARAM, lParam: LPARAM) -> LRESULT;
+    pub fn GetMessageW(lpMsg: *mut MSG, hWnd: HWND, wMsgFilterMin: UINT, wMsgFilterMax: UINT) -> BOOL;
+    pub fn PostMessageW(hWnd: HWND, Msg: UINT, wParam: WPARAM, lParam: LPARAM) -> BOOL;
+    pub fn SendMessageW(hWnd: HWND, Msg: UINT, wParam: WPARAM, lParam: LPARAM) -> LRESULT;
+    pub fn DispatchMessageW(lpmsg: *const MSG) -> LRESULT;
+    pub fn PostQuitMessage(nExitCode: c_int);
+
+    // Class/window storage manipulation
+    pub fn GetClassLongW(hWnd: HWND, nIndex: c_int) -> DWORD;
+    pub fn SetClassLongW(hWnd: HWND, nIndex: c_int, dwNewLong: LONG) -> DWORD;
+    pub fn GetWindowLongW(hWnd: HWND, nIndex: c_int) -> LONG;
+    pub fn SetWindowLongW(hWnd: HWND, nIndex: c_int, dwNewLong: LONG) -> LONG;
+    #[cfg(target_pointer_width = "64")]
+    pub fn GetClassLongPtrW(hWnd: HWND, nIndex: c_int) -> ULONG_PTR;
+    #[cfg(target_pointer_width = "64")]
+    pub fn SetClassLongPtrW(hWnd: HWND, nIndex: c_int, dwNewLong: LONG_PTR) -> ULONG_PTR;
+    #[cfg(target_pointer_width = "64")]
+    pub fn GetWindowLongPtrW(hWnd: HWND, nIndex: c_int) -> LONG_PTR;
+    #[cfg(target_pointer_width = "64")]
+    pub fn SetWindowLongPtrW(hWnd: HWND, nIndex: c_int, dwNewLong: LONG_PTR) -> LONG_PTR;
+}
+
+// These functions are #define'd as one or the other based on arch in the Win32 headers.
+// Unfortunately, their signatures do not match, so it's better to rewrap it like this.
+// Both LONG and LONG_PTR are equivalent to the size of usize on their respective targets.
+#[cfg(target_pointer_width = "32")]
+pub unsafe fn get_class_data(hwnd: HWND, offset: c_int) -> usize {
+    GetClassLongW(hwnd, offset) as usize
+}
+#[cfg(target_pointer_width = "64")]
+pub unsafe fn get_class_data(hwnd: HWND, offset: c_int) -> usize {
+    GetClassLongPtrW(hwnd, offset) as usize
+}
+#[cfg(target_pointer_width = "32")]
+pub unsafe fn set_class_data(hwnd: HWND, offset: c_int, data: usize) -> usize {
+    SetClassLongW(hwnd, offset, data as LONG) as usize
+}
+#[cfg(target_pointer_width = "64")]
+pub unsafe fn set_class_data(hwnd: HWND, offset: c_int, data: usize) -> usize {
+    SetClassLongPtrW(hwnd, offset, data as LONG_PTR) as usize
+}
+#[cfg(target_pointer_width = "32")]
+pub unsafe fn get_window_data(hwnd: HWND, offset: c_int) -> usize {
+    GetWindowLongW(hwnd, offset) as usize
+}
+#[cfg(target_pointer_width = "64")]
+pub unsafe fn get_window_data(hwnd: HWND, offset: c_int) -> usize {
+    GetWindowLongPtrW(hwnd, offset) as usize
+}
+#[cfg(target_pointer_width = "32")]
+pub unsafe fn set_window_data(hwnd: HWND, offset: c_int, data: usize) -> usize {
+    SetWindowLongW(hwnd, offset, data as LONG) as usize
+}
+#[cfg(target_pointer_width = "64")]
+pub unsafe fn set_window_data(hwnd: HWND, offset: c_int, data: usize) -> usize {
+    SetWindowLongPtrW(hwnd, offset, data as LONG_PTR) as usize
+}
--- src/platform/win32.rs
+++ src/platform/win32.rs
@@ -1,12 +1,69 @@
 //! Windows API specific implementations and API extensions.
 
-use crate::window::WindowImpl;
+#[path = "bindings/win32.rs"]
+mod bindings;
+use self::bindings::*;
 
-pub(crate) struct Window {
-    // ...
+use crate::{
+    error::Error,
+    helpers::LazyCell,
+    window::{WindowBuilder, WindowImpl},
+};
+use std::{cell, ffi, mem, ops, ptr, thread};
+use std::sync::{Arc, Condvar, Mutex}; // move later
+
+/// Global lock used to synchronize classes being registered or queried.
+static CLASS_REGISTRY_LOCK: LazyCell<Mutex<()>> = LazyCell::new(Default::default);
+
+/// Retrieves the base module HINSTANCE.
+#[inline]
+fn this_hinstance() -> HINSTANCE {
+    extern "system" {
+        // Microsoft's linkers provide a static HINSTANCE to not have to query it at runtime.
+        // Source: https://devblogs.microsoft.com/oldnewthing/20041025-00/?p=37483
+        // (I love you Raymond Chen)
+        static __ImageBase: [u8; 64];
+    }
+    (unsafe { &__ImageBase }) as *const [u8; 64] as HINSTANCE
 }
 
+fn widen_string(s: &str) -> impl Iterator<Item = WCHAR> + '_ {
+    use std::os::windows::ffi::OsStrExt;
+    <str as AsRef<ffi::OsStr>>::as_ref(s)
+        .encode_wide()
+        .chain(Some(0x00))
+}
+
+pub(crate) struct Window {
+    // guts
+    class: ATOM,
+    hwnd: HWND,
+    thread: Option<thread::JoinHandle<()>>,
+
+    // api
+    user_data: Box<cell::UnsafeCell<WindowUserData>>,
+}
+unsafe impl Send for Window {}
+unsafe impl Sync for Window {}
+
 pub(crate) type WindowRepr = Window;
+
+struct WindowCreateParams {
+    builder_ptr: *const WindowBuilder,
+    user_data_ptr: *mut WindowUserData,
+}
+
+struct WindowUserData {
+    destroy_class: bool,
+}
+
+impl Default for WindowUserData {
+    fn default() -> Self {
+        Self {
+            destroy_class: false,
+        }
+    }
+}
 
 impl WindowImpl for Window {
     fn set_visible(&self, visible: bool) {
@@ -15,5 +72,180 @@ impl WindowImpl for Window {
 
     fn swap_events(&mut self) {
         // ...
+    }
+}
+
+unsafe extern "system" fn window_proc(hwnd: HWND, msg: UINT, wparam: WPARAM, lparam: LPARAM) -> LRESULT {
+    unsafe fn user_data<'a>(hwnd: HWND) -> &'a mut WindowUserData {
+        &mut *(get_window_data(hwnd, GWLP_USERDATA) as *mut WindowUserData)
+    }
+
+    match msg {
+        WM_DESTROY => {
+            PostQuitMessage(0);
+            0
+        },
+
+        WM_CLOSE => {
+            DestroyWindow(hwnd);
+            0
+        },
+
+        WM_NCCREATE => {
+            // `lpCreateParams` is the first member, so `CREATESTRUCTW *` is `WindowCreateParams **`
+            let params = &mut **(lparam as *const *mut WindowCreateParams);
+
+            // Store user data pointer
+            set_window_data(hwnd, GWLP_USERDATA, params.user_data_ptr as usize);
+
+            DefWindowProcW(hwnd, msg, wparam, lparam)
+        },
+
+        WM_NCDESTROY => {
+            // finalize
+            DefWindowProcW(hwnd, msg, wparam, lparam)
+        },
+        _ => DefWindowProcW(hwnd, msg, wparam, lparam),
+    }
+}
+
+pub(crate) fn make_window(builder: &WindowBuilder) -> Result<WindowRepr, Error> {
+    let signal = Arc::new((Mutex::<Option<Result<WindowRepr, Error>>>::new(None), Condvar::new()));
+
+    let builder = builder.clone();
+    let cond_pair = Arc::clone(&signal);
+    let window_thread = thread::spawn(move || unsafe {
+        // TODO: Sanitize reserved window classes
+        let mut class_info = mem::MaybeUninit::<WNDCLASSEXW>::uninit();
+        let class_name = widen_string(builder.__class_name.as_ref()).collect::<Vec<_>>();
+        (&mut *class_info.as_mut_ptr()).cbSize = mem::size_of_val(&class_info) as DWORD;
+
+        // Create the window class if it doesn't exist yet
+        let class_atom: ATOM;
+        let class_registry_lock = CLASS_REGISTRY_LOCK.lock().unwrap();
+        if GetClassInfoExW(this_hinstance(), class_name.as_ptr(), class_info.as_mut_ptr()) == 0 {
+            // The window class not existing sets the thread global error flag,
+            // we clear it immediately to avoid any confusion down the line.
+            SetLastError(ERROR_SUCCESS);
+
+            // Fill in & register class (cbSize is set before this `if` block)
+            let class = &mut *class_info.as_mut_ptr();
+            class.style = CS_OWNDC;
+            class.lpfnWndProc = window_proc;
+            class.cbClsExtra = 0;
+            class.cbWndExtra = 0;
+            class.hInstance = this_hinstance();
+            class.hIcon = ptr::null_mut();
+            class.hCursor = ptr::null_mut();
+            class.hbrBackground = ptr::null_mut();
+            class.lpszMenuName = ptr::null_mut();
+            class.lpszClassName = class_name.as_ptr();
+            class.hIconSm = ptr::null_mut();
+
+            class_atom = RegisterClassExW(class);
+            if class_atom == 0 {
+                todo!("handle the class not registering")
+            }
+        } else {
+            // If the class already exists, query the atom for the class name
+            class_atom = GlobalFindAtomW(class_name.as_ptr());
+        }
+        mem::drop(class_registry_lock);
+
+        // TODO: what
+        assert_ne!(class_atom, 0);
+
+        // `class_name` is no longer needed, as `class_atom` maps to a copy managed by the OS
+        mem::drop(class_name);
+
+        let style = WS_OVERLAPPEDWINDOW | WS_VISIBLE;
+        let style_ex = 0;
+
+        let width = 1280;
+        let height = 720;
+        let title = widen_string(builder.__title.as_ref()).collect::<Vec<_>>();
+        let user_data: Box<cell::UnsafeCell<WindowUserData>> = Default::default();
+
+        let builder_ptr = (&builder) as *const WindowBuilder;
+        let user_data_ptr = user_data.as_ref().get();
+        let mut params = WindowCreateParams { builder_ptr, user_data_ptr };
+
+        let hwnd = CreateWindowExW(
+            style_ex,
+            class_atom as LPCWSTR,
+            title.as_ptr(),
+            style,
+            CW_USEDEFAULT,
+            CW_USEDEFAULT,
+            width,
+            height,
+            ptr::null_mut(),
+            ptr::null_mut(),
+            this_hinstance(),
+            (&mut params) as *mut WindowCreateParams as LPVOID,
+        );
+
+        // TODO: if hwnd bad
+
+        // Yield window struct, signal outer function
+        let (mutex, condvar) = &*cond_pair;
+        let mut lock = mutex.lock().unwrap();
+        *lock = Some(Ok(Window {
+            class: class_atom,
+            hwnd,
+            thread: None,
+            user_data,
+        }));
+        condvar.notify_one();
+        mem::drop(lock);
+
+        // Release condvar + mutex pair so the Arc is deallocated once the outer function returns
+        mem::drop(cond_pair);
+
+        // Run message loop until error or exit
+        let mut msg = mem::MaybeUninit::<MSG>::zeroed().assume_init();
+        'message_loop: loop {
+            // `HWND hWnd` is set to NULL here to query all messages on the thread,
+            // as the exit condition/signal (WM_QUIT) is not associated with any window.
+            // This is one of the main motivations (besides no blocking) to give each window a thread.
+            match GetMessageW(&mut msg, ptr::null_mut(), 0, 0) {
+                -1 => panic!("Hard error {:#06X} in GetMessageW loop!", GetLastError()),
+                0 => break 'message_loop,
+                _ => {
+                    // Dispatch message to WindowProc
+                    DispatchMessageW(&msg);
+                },
+            }
+        }
+
+        // Registered window classes are unregistered automatically when the process closes.
+        // Until then, there's no reason not to have them around as the contents never vary.
+        // > if something { UnregisterClassW(class_atom); }
+
+        // TODO: Don't do this, obviously.
+        ExitProcess(0);
+    });
+
+    // Wait until the thread is done creating the window or notifying us why it couldn't do that
+    let (mutex, condvar) = &*signal;
+    let mut lock = mutex.lock().unwrap();
+    loop {
+        if let Some(result) = lock.take() {
+            break result.map(|mut window| {
+                window.thread = Some(window_thread);
+                window
+            })
+        } else {
+            lock = condvar.wait(lock).unwrap();
+        }
+    }
+}
+
+impl ops::Drop for Window {
+    fn drop(&mut self) {
+        // unsafe {
+        //     // TODO: X_WM_CLOSE, etc
+        // }
+        let _ = self.thread.take().map(thread::JoinHandle::join);
     }
 }
