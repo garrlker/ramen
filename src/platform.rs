f7d0f54b4c5c2eef9c73694465ac8c04ec30a3c1
viri <hi@viri.moe>
2020-12-23 00:22:21 -0600

    platform: document `imp` for window creation

--- src/platform.rs
+++ src/platform.rs
@@ -5,7 +5,7 @@
 //
 // To implement your own platforms, here's what you need to export:
 // - The type `WindowRepr` that derefs to or implements `WindowImpl` (is callable as such).
-// - ...
+// - The function `make_window` of type `fn(&WindowBuilder) -> Result<WindowRepr, Error>`
 
 #[cfg(windows)]
 pub mod win32;
