5bebd861beed1572004961f1e754bfd0e7bb6c92
viri <hi@viri.moe>
2020-12-23 04:03:06 -0600

    event: add `Event`, `CloseReason`

---
+++ src/event.rs
@@ -0,0 +1,23 @@
+//! the event api
+
+pub enum Event {
+    CloseRequest(CloseReason),
+}
+
+#[derive(Debug, Copy, Clone, Eq, PartialEq)]
+pub enum CloseReason {
+    /// The user has pressed a system control to close the window.
+    ///
+    /// This is usually the "X button" or the red stop light on the control menu.
+    SystemMenu,
+
+    /// The user has pressed the native keyboard shortcut to close the active window.
+    ///
+    /// This is usually Alt+F4, Command+W, or Control+W.
+    KeyboardShortcut,
+
+    /// The reason for the close request is unknown.
+    ///
+    /// Likely reasons include external programs sending the signal.
+    Unknown,
+}
